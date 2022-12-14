c8d1e57ada7236bf404df530bfebfdb6cc0924ac
hryhorii.voloshyn <grigoriyvoloshyn@gmail.com>
2022-12-14 21:31:40 +0200

    Init

---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,11 @@
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+
+export default function SignIn() {
+  return (
+    <Box sx={{ mt: 1 }}>
+      <TextField />
+      <TextField type="password" />
+    </Box>
+  );
+}
